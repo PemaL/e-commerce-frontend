47bf1a7d3dd62ce954b8e536d762e094f0395f33
PemaL <pema.srp72@gmail.com>
2022-01-04 17:20:42 -0500

    added routes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,57 @@
-import SignUp from '../components/signup'
 
-function Home(){
-  return(
-     <>
-       <SignUp/> 
-     </>
-  )
- 
+
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  CssBaseline,
+  Typography,
+  makeStyles,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  navlinks: {
+    marginLeft: theme.spacing(10),
+    display: "flex",
+  },
+ logo: {
+    flexGrow: "1",
+    cursor: "pointer",
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+}));
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <CssBaseline />
+      <Toolbar>
+        <Typography variant="h4" className={classes.logo}>
+        Dom's Shop
+        </Typography>
+          <div className={classes.navlinks}>
+            <Link to="/SignUp" className={classes.link}>
+              Sign Up
+            </Link>
+            <Link to="/SignIn" className={classes.link}>
+              Sign In
+            </Link>
+        
+          </div>
+      </Toolbar>
+    </AppBar>
+  );
 }
-
-export default Home 
+export default Home;
